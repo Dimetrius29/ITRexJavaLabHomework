d049ff10491f664c162691dd773e9b93a4fac426
Dimetrius29 <dimetrius2991@gmail.com>
2021-09-19 23:35:17 +0300

    Ex4

---
+++ src/Java/Exercise4/Task1.java
@@ -0,0 +1,37 @@
+package Java.Exercise4;
+
+/**
+ * You need to write your own functional interface (TernaryIntPredicate) and use it with a
+ * lambda expression.
+ * The interface must have a single non-static (and non-default) method test with
+ * three int arguments that returns boolean value.
+ * Besides, you need to write a lambda expression with three int arguments using your
+ * TernaryIntPredicate.
+ * <p>
+ * The lambda expression has to return true if all passed values are different otherwise false. The name of the instance is allValuesAreDifferentPredicate. It should be a static field.
+ * <p>
+ * Sample Input 1:
+ * 1 1 1
+ * <p>
+ * Sample Output 1:
+ * False
+ * <p>
+ * Sample Input 2:
+ * 2 3 4
+ * <p>
+ * Sample Output 2:
+ * True
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println(allValuesAreDifferentPredicate.test(1,2,3));
+    }
+
+    public static final TernaryIntPredicate allValuesAreDifferentPredicate = (x, y, z) -> x != y && y != z && x != z;
+
+    @FunctionalInterface
+    public interface TernaryIntPredicate {
+        boolean test(int x, int y, int z);
+    }
+}
---
+++ src/Java/Exercise4/Task6.java
@@ -0,0 +1,31 @@
+package Java.Exercise4;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
+
+/**
+ * <p>
+ * Write a curried form of the function f(x,y,z)=x+y∗y+z∗z∗z using lambda expressions in Java >8 style.
+ * The result and x, y, z must be integer numbers.
+ * <p>
+ * Write a curried function (using lambdas) that accepts four string arguments and concatenated all in one string
+ * following the rules:
+ * String cases: in the result string, first and second arguments must be in lower cases and third and fourth in
+ * upper cases.
+ * Order of arguments concatenation: first, third, second, fourth.
+ */
+public class Task6 {
+
+    public static void main(String[] args) {
+        Function<Integer, Function<Integer, Function<Integer, Integer>>> function1 = x -> y -> z -> x + y * y + z * z * z;
+        Function<String, Function<String, Function<String, Function<String, String>>>> function2 = a -> b -> c -> d -> new StringBuilder(a.toLowerCase())
+                .append(c.toUpperCase())
+                .append(b.toLowerCase())
+                .append(d.toUpperCase())
+                .toString();
+
+        System.out.println(function1.apply(2).apply(3).apply(4));
+        System.out.println(function2.apply("Hello").apply("Hi").apply("world").apply("Jack"));
+    }
+}
